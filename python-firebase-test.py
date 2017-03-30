6872948b1cb21712430cd2f3f5634193275a13c0
hamzahppa <hamzahibnabdullah@gmail.com>
2017-03-30 19:13:05 +0700

    final_result

--- python-firebase-test.py
+++ python-firebase-test.py
@@ -8,11 +8,11 @@ r_earth = 6378
 pi = 3.1415
 
 # define user lat long
-lat = -7.5655367
-lng = 110.8085373
+lat = -7.5651697
+lng = 110.8102488
 
 # define boundary within 5 km of the location
-bound = 0.10
+bound = 0.20
 new_lat1  = lat  + (bound / r_earth) * (180 / pi);
 new_lat2  = lat  - (bound / r_earth) * (180 / pi);
 new_lng1 = lng + (bound / r_earth) * (180 / pi) / math.cos(lat * pi/180);
@@ -20,9 +20,12 @@ new_lng2 = lng - (bound / r_earth) * (180 / pi) / math.cos(lat * pi/180);
 print new_lat1, new_lat2, new_lng1, new_lng2
 
 result = firebase.get('/dataResto', None, {'orderBy' : '"map/long"', 'startAt' : new_lng2, 'endAt' : new_lng1})
-print result
+final_result = {}
+
 # see data.py
 for x, restoran in result.items():
-	for y, attr in restoran.items():
-		print y, attr
+	if result[x]['map']['lat'] > new_lat2 and result[x]['map']['lat'] < new_lat1 :
+		final_result.update(result[x])
 # will print retrieved data
+
+print final_result;
